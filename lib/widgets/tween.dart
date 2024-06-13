9cd01eb97db19a6feca606eaf3a24744e7d05aca
Rokeya17 <rokeyayasminmim17official@gmail.com>
2024-06-13 14:16:21 +0600

    flutter widgets practices

---
+++ lib/widgets/tween.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TweenWidgtScreen extends StatelessWidget {
+  const TweenWidgtScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
